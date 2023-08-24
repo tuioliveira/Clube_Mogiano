c5a4ef0bc745ae9bbb02ecd9acc2b61447c4865c
Arthur Barbosa de Oliveira <arthurvl98@gmail.com>
2023-08-24 09:19:24 -0300

    Trabalhando no menu e tela inicial

---
+++ app/src/main/java/br/com/clubemogiano/startscreen/StartScreenActivity.kt
@@ -0,0 +1,46 @@
+package br.com.clubemogiano.startscreen
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import br.com.clubemogiano.R
+import br.com.clubemogiano.databinding.ActivityStartScreenBinding
+
+class StartScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        binding = ActivityStartScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.apply {
+            title = "Clube Mogiano"
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_start,menu);
+
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_social -> {
+                // Handle settings menu item click
+                true
+            }
+            // Add more cases for other menu items
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
