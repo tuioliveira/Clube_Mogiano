72c41b5590386f9d22f2c77d9ad8b821c94248c6
Arthur Barbosa de Oliveira <arthurvl98@gmail.com>
2023-09-06 08:10:45 -0300

    Task - Make a method to display a Toast and Place text on the layout programmatically

--- app/src/main/java/br/com/clubemogiano/presentation/PresentationActivity.kt
+++ app/src/main/java/br/com/clubemogiano/presentation/PresentationActivity.kt
@@ -1,5 +1,15 @@
 package br.com.clubemogiano.presentation
 
+import br.com.clubemogiano.databinding.ActivityPresentationBinding
+import android.content.Context
+import android.widget.Toast
+
+
 class PresentationActivity {
 
+    private lateinit var binding: ActivityPresentationBinding
+
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/br/com/clubemogiano/startscreen/StartScreenActivity.kt
+++ app/src/main/java/br/com/clubemogiano/startscreen/StartScreenActivity.kt
@@ -1,11 +1,15 @@
 package br.com.clubemogiano.startscreen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import br.com.clubemogiano.R
 import br.com.clubemogiano.databinding.ActivityStartScreenBinding
+import android.content.Context
+import android.widget.Toast
+import android.widget.TextView
 
 class StartScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStartScreenBinding
@@ -29,10 +33,11 @@ class StartScreenActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
-        menuInflater.inflate(R.menu.menu_start,menu);
+        menuInflater.inflate(R.menu.menu_start, menu);
 
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.item_social -> {
@@ -41,6 +46,20 @@ class StartScreenActivity : AppCompatActivity() {
             }
             // Add more cases for other menu items
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_start_screen)
+
+            val textView = findViewById<TextView>(R.id.wellcomeText)
+            textView.text = "Bem vindo ao app"
         }
     }
 }
