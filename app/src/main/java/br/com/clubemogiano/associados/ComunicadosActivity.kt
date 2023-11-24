383667d508658ffc8f9ae5bda0ef90ab5acba788
Arthur Barbosa de Oliveira <arthurvl98@gmail.com>
2023-11-24 11:57:39 -0300

    Concluido layout de Comunicados

--- app/src/main/java/br/com/clubemogiano/associados/ComunicadosActivity.kt
+++ app/src/main/java/br/com/clubemogiano/associados/ComunicadosActivity.kt
@@ -1,8 +1,12 @@
 package br.com.clubemogiano.associados
 
+import android.content.Context
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.clubemogiano.databinding.ActivityComunicadosBinding
+import br.com.clubemogiano.databinding.ActivitySportsBinding
 
 class ComunicadosActivity : AppCompatActivity() {
     private lateinit var binding: ActivityComunicadosBinding
@@ -16,5 +20,22 @@ class ComunicadosActivity : AppCompatActivity() {
     private fun init() {
         binding = ActivityComunicadosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarSports)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.apply {
+            title = "Comunicados"
+        }
     }
-}
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                android.R.id.home -> {
+                    onBackPressed()
+                    true
+                }
+
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+    }
